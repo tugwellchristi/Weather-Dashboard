14b35157a3a92b1024fe6fc464939e7745478791
tugwellchristi <tugwellchristi@gmail.com>
2023-12-10 13:31:26 -0800

    js is working, cities save and can be reset

--- Assets/JS/script.js
+++ Assets/JS/script.js
@@ -1,25 +1,71 @@
 const searchForm = document.getElementById("search-form");
-const searchInput = document.getElementById("searchedCity");
-const apiURL = "https://api.openweathermap.org/data/2.5/weather?q=";
-let city;
-const apiKey = "&appid=(API KEY GOES HERE)&units=imperial";
+const searchInput = document.getElementById("searchCity");
+const APIkey = "f4412f0abd4f656793c98f9a58d8c240";
+const searchedCitiesContainer = document.getElementById("searchedCities");
 
 function getCurrentWeather(city) {
     console.log(city);
-    var newURL = apiURL + city + apiKey;
+    var newURL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${APIkey}&units=imperial`;
     console.log(newURL);
+
     fetch(newURL)
-    .then(function(res) {
-        console.log(res);
-        return res.json();
-    })
+        .then(function (res) {
+            console.log(res);
+            return res.json();
+        })
         .then(function (data) {
             console.log(data);
+            const weatherInfo = `<p><strong>City: ${data.name}<strong></p><p>Temperature: ${data.main.temp}&deg;F</p>`;
+            document.getElementById("weatherDashboard").innerHTML = weatherInfo;
         })
+        .catch(function (error) {
+            console.error("Error fetching weather data", error);
+        });
 }
 
-function searchCity() {
-    city = searchInput.ariaValueMax.trim();
+function addCity() {
+    city = searchInput.value.trim();
     console.log(city);
-    getCurrentWeather(city);
+    
+    if (city !== "") {
+        saveCity(city);
+        displaySavedCities();
+        getCurrentWeather(city);
+
+        searchInput.value = "";
+    }
 }
+
+function saveCity(city) {
+    const savedCities = JSON.parse(localStorage.getItem("savedCities")) || [];
+
+    savedCities.push(city);
+    localStorage.setItem("savedCities", JSON.stringify(savedCities));
+}
+
+function displaySavedCities() {
+    const savedCities = JSON.parse(localStorage.getItem("savedCities")) || [];
+
+    searchedCitiesContainer.innerHTML = "";
+    savedCities.forEach(city => {
+        const cityButton = document.createElement("button");
+        cityButton.textContent = city;
+        cityButton.classList.add("button", "is-primary", "mt-1", "ml-5");
+        cityButton.addEventListener("click", function () {
+            getCurrentWeather(city);
+        });
+        searchedCitiesContainer.appendChild(cityButton);
+    })
+}
+
+searchForm.addEventListener("submit", function (event) {
+    event.preventDefault();
+    addCity();
+});
+
+function clearCitiesBtn() {
+    localStorage.removeItem("savedCities");
+    displaySavedCities();
+}
+
+displaySavedCities();
